de6a9a451fd8ad736171935b30e34be5fc0c9c8b
Mateus Levy <levymateus06@Gmail.com>
2023-07-10 22:04:07 -0300

    fixing header

--- src/pages/Help/index.jsx
+++ src/pages/Help/index.jsx
@@ -3,31 +3,31 @@ import OutputCard from "../../components/OutputCard";
 import { useOutputCardData } from "../../contexts/OutputCardContext";
 import ProductCard from "../../components/ProductCard";
 const Help = () => {
-    const { outputCardData }  = useOutputCardData()
-    if(outputCardData.loaded){
-        const getTotalAmount = ()=>{
-            let amount = 0; 
-            outputCardData.result.map((item)=>{
-                amount+=item.price;
+    const { outputCardData } = useOutputCardData()
+    if (outputCardData.loaded) {
+        const getTotalAmount = () => {
+            let amount = 0;
+            outputCardData.result.map((item) => {
+                amount += item.price;
 
             })
             return amount
         }
-        return ( 
+        return (
             <div className="product">
-            <h1>Produtos Sujeridos</h1>
-            <h1>Total: R$ {getTotalAmount()} </h1>
-            <div className="product-grid">
-                {outputCardData.result.map((product) =>
-                    <ProductCard key={product.id}  product={product} />
-                )}
+                <h1>Produtos Sujeridos</h1>
+                <h1>Total: R$ {getTotalAmount()} </h1>
+                <div className="product-grid">
+                    {outputCardData.result.map((product) =>
+                        <ProductCard key={product.id} product={product} />
+                    )}
+                </div>
             </div>
-        </div>
         )
     }
     return (
         <>
-            <OutputCard/>
+            <OutputCard />
         </>
     )
 
